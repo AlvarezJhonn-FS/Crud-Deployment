f1bebd97435a95f751c5368c95f6227c5d99ba70
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-12-06 10:20:31 -0500

    Created serversjs

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require ('express');
 require('dotenv'). config();
+const mongoose = require('mongoose');
+const path = require('path');
+const cors =require('cors');
+
+
 
 const app = express();
 
@@ -8,6 +13,16 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection;
+db.on('error', error => console.error(error) );
+db.once('open', () => console.log('Database Connected'));
+
+const movieRouter  = require ('./routes/movies');
+app.use('/movies', movieRouter);
+
 app.listen(PORT, () => {
-    console.log(`sserver is running in port ${PORT}`)
+    console.log(`server is running on port ${PORT}`)
 } )
