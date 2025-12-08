2e0d190f818ed37cd2cf9992b0790904444f1fcc
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-12-07 20:50:15 -0500

    Updated server.js for the routes to the movies and started my mongoose connections

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require ('express');
 require('dotenv'). config();
+
+console.log("Loaded .env from:", __dirname);
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
 const mongoose = require('mongoose');
 const path = require('path');
 const cors =require('cors');
@@ -14,14 +18,15 @@ app.use(express.json());
 const PORT = process.env.PORT || 8000;
 
 const DATABASE_URL = process.env.DATABASE_URL;
-
-mongoose.connect(DATABASE_URL, {useNewUrlParser: true})
+const movieRouter  = require ('./routes/movie');
+mongoose.connect(DATABASE_URL)
 const db = mongoose.connection;
 db.on('error', error => console.error(error) );
 db.once('open', () => console.log('Database Connected'));
 
-const movieRouter  = require ('./routes/movies');
-app.use('/movies', movieRouter);
+
+app.use(express.json());
+app.use('/movie', movieRouter);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
