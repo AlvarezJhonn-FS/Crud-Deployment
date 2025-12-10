543b17ab304dec9f153a872e632d5491e194f126
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-12-10 18:22:33 -0500

    Updated port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // app.use(cors.json())
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const movieRouter  = require ('./routes/movie');
