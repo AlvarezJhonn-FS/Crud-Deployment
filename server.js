5f98c11e5f987233d1b13ab713e5be6415025e51
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-12-15 18:09:05 -0500

    Fix CORS and API route

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const express = require ('express');
-require('dotenv'). config();
-// const path = require('path');
-// const cors = require('cors');
-
-console.log("Loaded .env from:", __dirname);
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
-
-const mongoose = require('mongoose');
-const path = require('path');
-const cors =require('cors');
-
-
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
-// app.use(cors.json())
 
+app.use(cors());
+
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-
 const DATABASE_URL = process.env.DATABASE_URL;
-const movieRouter  = require ('./routes/movie');
-mongoose.connect(DATABASE_URL)
+
+// Routes
+const movieRouter = require("./routes/movie");
+app.use("/movies", movieRouter);
+
+// Database
+mongoose.connect(DATABASE_URL);
 const db = mongoose.connection;
-db.on('error', error => console.error(error) );
-db.once('open', () => console.log('Database Connected'));
 
-
-app.use(express.json());
-app.use('/movie', movieRouter);
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Database Connected"));
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-} )
+  console.log(`Server running on port ${PORT}`);
+});
